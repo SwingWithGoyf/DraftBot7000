ffc6831f6fe4588f356480f70c5b7c8c83771184
SwingWithGoyf <34694139+SwingWithGoyf@users.noreply.github.com>
2018-07-07 08:17:17 -0700

    adding more modules, better program flow

--- app.js
+++ app.js
@@ -33,8 +33,10 @@ var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.
 var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
 
 // Create your bot with a function to receive messages from the user
-var bot = new builder.UniversalBot(connector);
-bot.set('storage', tableStorage);
+var bot = new builder.UniversalBot(connector, function(session) {
+    session.send("Welcome to draft bot 7000!  Here's some useful info: lorem ipsum etc");
+    session.endDialog();
+}).set('storage', tableStorage);
 
 // The dialog stack is cleared and this dialog is invoked when the user enters 'help'.
 bot.dialog('help', function (session, args, next) {
@@ -50,73 +52,6 @@ bot.dialog('help', function (session, args, next) {
     // }
 });
 
-// This dialog help the user order dinner to be delivered to their hotel room.
-var dinnerMenu = {
-    "Potato Salad - $5.99": {
-        Description: "Potato Salad",
-        Price: 5.99
-    },
-    "Tuna Sandwich - $6.89": {
-        Description: "Tuna Sandwich",
-        Price: 6.89
-    },
-    "Clam Chowder - $4.50":{
-        Description: "Clam Chowder",
-        Price: 4.50
-    }
-};
+require('./orderDinner.js')(bot, builder);
 
-bot.dialog('orderDinner', [
-    function(session){
-        session.send("Lets order some dinner!");
-        builder.Prompts.choice(session, "Dinner menu:", dinnerMenu);
-    },
-    function (session, results) {
-        if (results.response) {
-            var order = dinnerMenu[results.response.entity];
-            var msg = `You ordered: ${order.Description} for a total of $${order.Price}.`;
-            session.userData.order = order;
-            session.send(msg);
-            builder.Prompts.text(session, "What is your room number?");
-        } 
-    },
-    function(session, results){
-        if(results.response){
-            session.userData.room = results.response;
-            var msg = `Thank you. Your order will be delivered to room #${session.userData.room}`;
-            session.endDialog(msg);
-        }
-    }
-])
-.triggerAction({
-    matches: /^order dinner$/i,
-    confirmPrompt: "This will cancel your order. Are you sure?"
-});
-
-bot.dialog('/', [
-    function (session) {
-        for (var i = 0; i < session.message.entities.length; i++) {
-            session.send(`Entity #${i}: ${session.message.entities[i]}`);
-        }
-        // capture session user information
-        session.userData = {"userId": session.message.user.id, "jobTitle": "Senior Developer"};
-        builder.Prompts.text(session, "Hello... What's your handle, sassafras?");
-    },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been derfing? (Btw, your id is " + session.userData.userId + " and your title is " + session.userData.jobTitle + ")"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been derfing for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
-    }
-])
-.triggerAction({
-    matches: /^hello$/i|/^hi$/i|/^yo$/i|/^sup$/i
-});
+require('./hello.js')(bot, builder);
---
+++ hello.js
@@ -0,0 +1,28 @@
+module.exports = function(bot, builder) {
+    bot.dialog('hello', [
+        function (session) {
+            for (var i = 0; i < session.message.entities.length; i++) {
+                session.send(`Entity #${i}: ${session.message.entities[i]}`);
+            }
+            // capture session user information
+            session.userData = {"userId": session.message.user.id, "jobTitle": "Senior Developer"};
+            builder.Prompts.text(session, "Hello... What's your handle, sassafras?");
+        },
+        function (session, results) {
+            session.userData.name = results.response;
+            builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been derfing? (Btw, your id is " + session.userData.userId + " and your title is " + session.userData.jobTitle + ")"); 
+        },
+        function (session, results) {
+            session.userData.coding = results.response;
+            builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+        },
+        function (session, results) {
+            session.userData.language = results.response.entity;
+            var msg = `Got it... ${session.userData.name} you've been derfing for ${session.userData.coding} years and use ${session.userData.language}.`;
+            session.endDialog(msg);
+        }
+    ])
+    .triggerAction({
+        matches: [/^hello$/i, /^hi$/i , /^yo$/i, /^sup$/i]
+    });
+};
